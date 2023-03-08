88b9cba8d949911cb4289815ef7a2f43406e90f2
SuhasLingam <95020208+SuhasLingam@users.noreply.github.com>
2023-03-09 00:05:14 +0530

    Password Cracker :white_check_mark:

---
+++ PasswordCracker.py
@@ -0,0 +1,28 @@
+import hashlib
+
+
+flag = 0
+pass_hash = input("Enter a MD5 Hash To Crack : \n")
+
+wordlist = input("File Name : \n")
+
+try:
+    pass_file = open(wordlist , "r")
+
+except:
+    print("File Not Found!!!")
+    quit()    
+
+for word in pass_file:
+    enc_word = word.encode('utf-8')
+    digest = hashlib.md5(enc_word.strip()).hexdigest()
+    
+    
+if digest == pass_hash:
+    print("Password Found")
+    print("Password is : " + word)
+    flag = 1
+    
+if flag == 0:
+    print("Password Not Found")    
+            
